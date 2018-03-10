f9b334151fe7561d7db6af85b79f5eb5bc88d514
hMatoba <hiroaki.mtb@outlook.com>
2018-03-10 12:27:02 +0900

    m

--- GetRss/index.js
+++ GetRss/index.js
@@ -5,16 +5,14 @@ module.exports = function (context, myTimer) {
     
     if (myTimer.isPastDue)
     {
-        (async () => {
-            const url = 'http://jvndb.jvn.jp/ja/rss/jvndb.rdf';
-            await storeRss(url);
+        context.log('JavaScript is running late!');
+    }
 
-            context.log('JavaScript timer trigger function ran!', timeStamp);       
-            context.done();
-        })();
-    }
-    else
-    {
+    (async () => {
+        const url = 'http://jvndb.jvn.jp/ja/rss/jvndb.rdf';
+        await storeRss(url);
+
+        context.log('JavaScript timer trigger function ran!', timeStamp);       
         context.done();
-    }
+    })();
 };
